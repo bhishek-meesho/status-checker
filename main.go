be5bd46d7b3a48cfaee5b54b7f35e6218d895d01
Bhishek Parmar <bhishek.parmar@meesho.com>
2025-05-21 16:48:00 +0530

    refactor code

--- main.go
+++ main.go
@@ -15,9 +15,10 @@ func main() {
 		"https://www.youtube.com",
 	}
 
+	//create a channel -> a pipe that allows us to send and receive messages between goroutines
+	ch := make(chan string)
+
 	for _, link := range links {
-		//create a channel -> a pipe that allows us to send and receive messages between goroutines
-		ch := make(chan string)
 
 		//launch a goroutine -> a lightweight thread managed by the Go runtime and
 		// channel is passed as an argument to the function - this allows the function to send a message back to the main goroutine
@@ -25,13 +26,13 @@ func main() {
 		// if we don't pass the channel, the function will not be able to send a message back to the main goroutine
 		// the channel is used to communicate between the main and the goroutine
 
-		//receive a message from the channel
-		// <- is the receive operator
-		msg := <-ch // this is the message from the channel
-
-		// print the message
-		fmt.Println(msg)
 	}
+	//receive a message from the channel
+	// <- is the receive operator
+	msg := <-ch // this is the message from the channel
+
+	// print the message
+	fmt.Println(msg)
 
 }
 
